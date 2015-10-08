0a277844661be1d9a98344bfce3d94e54bc79c1b
Geoffrey Oxberry <oxberry1@llnl.gov>
2015-10-07 19:10:29 -0700

    Add CMake toolchain file for Sierra.

---
+++ LLNL-Sierra.cmake
@@ -0,0 +1,8 @@
+SET(CMAKE_C_COMPILER mpicc)
+SET(CMAKE_CXX_COMPILER mpicxx)
+SET(CMAKE_Fortran_COMPILER mpif90)
+
+SET(Boost_NO_BOOST_CMAKE TRUE)
+SET(BOOST_ROOT /usr/gapps/ppp/boost-nompi-1.57.0)
+SET(Boost_LIBRARY_DIRS /usr/gapps/ppp/boost-nompi-1.57.0/lib)
+SET(Boost_INCLUDE_DIRS /usr/gapps/ppp/boost-nompi-1.57.0/include)
